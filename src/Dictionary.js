1b6874e487d38f3bd0bf8d76c5c95558e33267bd
SomayaAl <102401317+SomayaAl@users.noreply.github.com>
2022-06-26 18:08:35 -0400

    Added form Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,1 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
+
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert({keyword});
+  }
+
+  function handleKeywordChange(event) {
+      setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Enter a word"
+          autoFocus={true}
+          autoComplete={false}
+          onChange={handleKeywordChange}
+        />
+        <input type="submit" text="Search"/>
+      </form>
+    </div>
+    
+  );
+}
