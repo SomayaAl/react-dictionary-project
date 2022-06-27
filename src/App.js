8974bca8471b7e28f2e998b9f7ad3f78eac45908
SomayaAl <102401317+SomayaAl@users.noreply.github.com>
2022-06-26 21:22:20 -0400

    Added footer and link to github

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import banner from "./dictionary-banner.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"><img src={banner} className="App-banner img-fluid" alt="banner"/></header>
+      <header className="App-header">
+        <img src={banner} className="App-banner img-fluid" alt="banner" />
+      </header>
       <Dictionary />
+      <footer>
+        This project was coded by Somaya Albadawi and is{" "}
+        <a
+          href="https://github.com/SomayaAl/react-dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced.
+        </a>
+      </footer>
     </div>
   );
 }
