107c2be8957a01da505d50bee80c36dbc87bc1fe
SomayaAl <102401317+SomayaAl@users.noreply.github.com>
2022-06-27 20:16:55 -0400

    Added photos api to dictionary app

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,25 +2,36 @@ import axios from "axios";
 import React, { useState } from "react";
 import "./App.css";
 import Results from "./Results.js";
+import Photos from "./Photos.js";
 
 export default function Dictionary(props) {
   let [word, setWord] = useState(props.defaultWord);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey =
+      "563492ad6f91700001000001620bbddc88ac45e5a39e75e6c7f654d1";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
-
   function handleSubmit(event) {
-      event.preventDefault();
-      search();
+    event.preventDefault();
+    search();
   }
 
   function handleWordChange(event) {
@@ -28,31 +39,32 @@ export default function Dictionary(props) {
   }
 
   function load() {
-      setLoaded(true);
-      search();
+    setLoaded(true);
+    search();
   }
 
   if (loaded) {
-  return (
-    <div className="Dictionary">
-      <div className="form-section">
-        <h3>What word would you like to look up?</h3>
-        <form onSubmit={handleSubmit}>
-          <input
-            className="form-control"
-            type="search"
-            defaultValue={props.defaultWord}
-            autoFocus="on"
-            autoComplete="off"
-            onChange={handleWordChange}
-          />
-        </form>
+    return (
+      <div className="Dictionary">
+        <div className="form-section">
+          <h3>What word would you like to look up?</h3>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="form-control"
+              type="search"
+              defaultValue={props.defaultWord}
+              autoFocus="on"
+              autoComplete="off"
+              onChange={handleWordChange}
+            />
+          </form>
+        </div>
+        <Results results={results} />
+        <Photos photos={photos} />
       </div>
-      <Results results={results} />
-    </div>
-  );
-} else {
-    load()
-    return "Loading..."
-}
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
--- src/Results.js
+++ src/Results.js
@@ -8,7 +8,9 @@ export default function Results(props) {
       <div>
         <div className="word-section">
           <div className="Results">
-            <h2><strong>{props.results.word}</strong></h2>
+            <h2>
+              <strong>{props.results.word}</strong>
+            </h2>
             {props.results.phonetics.map(function (phonetic, index) {
               return (
                 <div key={index}>
